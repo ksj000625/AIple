cba1283634f5a6f6a976c657ca1bb5565cbc629f
Kim Seongjin <darkmoon625@naver.com>
2023-12-24 20:53:26 +0900

    Initialize Project

---
+++ src/main/java/projectAIple/AIple/AIpleApplication.java
@@ -0,0 +1,13 @@
+package projectAIple.AIple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AIpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AIpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projectAIple/AIple/AIpleApplicationTests.java
@@ -0,0 +1,13 @@
+package projectAIple.AIple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AIpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
