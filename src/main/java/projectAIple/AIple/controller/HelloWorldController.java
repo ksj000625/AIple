5fd210e9e1b6776513cb6f2bd5af91963ccbf59a
Kim Seongjin <darkmoon625@naver.com>
2024-01-04 18:17:21 +0900

    correct spell

--- src/main/java/projectAIple/AIple/controller/HelloWorldController.java
+++ src/main/java/projectAIple/AIple/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package projectAIple.AIple.Controller;
+package projectAIple.AIple.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
