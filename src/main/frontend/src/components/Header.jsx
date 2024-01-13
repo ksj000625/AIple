baf019f65f199ef5a4d918dd948a8af2a6901cb5
Dindory <22jysong@gmail.com>
2024-01-13 13:29:50 +0900

    헤더 컴포넌트 추가
    - Header 컴포넌트 생성
    - 불필요한 svg 삭제

---
+++ src/main/frontend/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+    return (
+        <div className="header-container">
+            <div className="header-wrap">
+                <div className="header-left-wrap">
+                    <Link className="logo" to='/'>
+                        Home
+                    </Link>
+                </div>
+                <ul>
+                    <li>
+                        <Link className="header-nav-item" to="/signup">
+                            가입
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className="header-nav-item" to="/signup/client">
+                            고객 가입
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className="header-nav-item" to="/signup/designer">
+                            디자이너 가입
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
