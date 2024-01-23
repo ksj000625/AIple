dec41606b5f8e25887d223d9d2816f06d535794c
Kim Seongjin <darkmoon625@naver.com>
2024-01-23 19:17:18 +0900

    correct pattern

--- src/main/frontend/src/pages/DesignerSignup.jsx
+++ src/main/frontend/src/pages/DesignerSignup.jsx
@@ -246,10 +246,10 @@ export default function DesignerSignup() {
                                                     : "false"
                                                 : undefined}
                                             {...register("phoneNumber", {
-                                                // pattern: {
-                                                // value: /^01(?:0|1|[6-9])(?:\d{3}|\d{4})\d{4}$/,
-                                                // message: "휴대폰 번호 형식에 맞지 않습니다.",
-                                                // },
+                                                pattern: {
+                                                value: /^\+[0-9]{1,3}\d{1,3}\d{1,4}\d{1,4}$/,
+                                                message: "휴대폰 번호 형식에 맞지 않습니다.",
+                                                },
                                             })}
                                         />{" "}
                                         {
